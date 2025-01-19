d16916eee5931af2f331b0bd6e35966e964365ac
deval <deval2299@gmail.com>
2025-01-19 09:34:14 +0530

    Main page changed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,36 @@
-<script>
-</script>
-
 <main>
+    <div class="image">image</div>
+    <div class="tc">
+        <div class="text">
+            <h1 style="font-size: 80px; text-align: justify; ">Find Your Perfect Space</h1>
+            <h2 style="font-size: 30px; text-decoration: underline; text-align: right; padding: 0; margin: 0;">Zero Fees, Zero Hassles, Zero Brokers</h2>
+        </div>
+        <div class="contact">contact us</div>
+    </div>
 </main>
 
 <style>
-    
+    main {
+        display: flex;
+        height: calc(100vh - 50px);
+        box-sizing: border-box;
+    }
+    .image{
+        width: 40vw;
+    }
+    .tc {
+        display: flex;
+        height: 100%;
+        width: 50vw;
+        flex-direction: column;
+    }
+    .text {
+        height: 60%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .contact{
+        height: 40%;
+    }
 </style>
